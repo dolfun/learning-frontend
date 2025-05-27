132ee0b0321d431c96f124e0b0f88a81992f6c84
Abhinav <abhinav.anand@sprinklr.com>
2025-05-27 10:35:08 +0530

    Move hello-js script into HTML itself

--- src/examples/hello-js.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("submitButton");
-button.addEventListener("click", () => {
-    const input = document.getElementById("nameInput");
-    const name = input.value;
-    
-    alert(`Hello ${name}, hope you're having a great day!`);
-});
